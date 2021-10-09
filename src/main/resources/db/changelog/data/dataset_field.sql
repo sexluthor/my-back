bf5efcf9c9ad3d5f7b5fb496b58e88f167680c27
burlkaovem <byres123@mail.ru>
2021-10-09 19:13:09 +0300

    add field type

--- src/main/resources/db/changelog/data/dataset_field.sql
+++ src/main/resources/db/changelog/data/dataset_field.sql
@@ -1,8 +1,8 @@
 -- Vtb clients
-insert into dataset_field (id, data_type, name, dataset_id, dataset_field_type) values
-	(1, 'BIGINT', 'user_id', 1, 'user_id'),
-	(2, 'VARCHAR', 'first_name', 1, 'first_name'),
-	(3, 'VARCHAR', 'last_name', 1, 'last_name'),
-	(4, 'DATE', 'birthday', 1, 'birthday'),
-	(5, 'VARCHAR', 'email', 1, 'email'),
-	(6, 'VARCHAR', 'phone', 1, 'phone');
+insert into dataset_field (id, data_type, name, dataset_id, dataset_field_type, is_primary) values
+	(1, 'BIGINT', 'user_id', 1, 'user_id', true),
+	(2, 'VARCHAR', 'first_name', 1, 'first_name', false),
+	(3, 'VARCHAR', 'last_name', 1, 'last_name', false),
+	(4, 'DATE', 'birthday', 1, 'birthday', false),
+	(5, 'VARCHAR', 'email', 1, 'email', false),
+	(6, 'VARCHAR', 'phone', 1, 'phone', false);
