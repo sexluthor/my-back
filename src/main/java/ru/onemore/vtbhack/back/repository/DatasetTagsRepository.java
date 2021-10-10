8a9cd766f4c974c77a1c560aa8bbd14c78f35407
burlkaovem <byres123@mail.ru>
2021-10-10 06:15:10 +0300

    tags

--- src/main/java/ru/onemore/vtbhack/back/repository/DatasetTagsRepository.java
+++ src/main/java/ru/onemore/vtbhack/back/repository/DatasetTagsRepository.java
@@ -1,6 +1,7 @@
 package ru.onemore.vtbhack.back.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import ru.onemore.vtbhack.back.jooq.tables.pojos.DatasetTag;
 
@@ -9,4 +10,6 @@ import java.util.List;
 @Repository
 public interface DatasetTagsRepository extends JpaRepository<DatasetTag, Long> {
 	List<DatasetTag> getAllByIdIn(List<Long> ids);
+	@Query(value = "select distinct name from dataset_tag", nativeQuery = true)
+	List<String> getDistinctName();
 }
--- src/main/java/ru/onemore/vtbhack/back/service/DatasetCatalogService.java
+++ src/main/java/ru/onemore/vtbhack/back/service/DatasetCatalogService.java
@@ -4,16 +4,20 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.onemore.vtbhack.back.dto.FilteredRequestDatasetDTO;
 import ru.onemore.vtbhack.back.entity.DatasetEntity;
+import ru.onemore.vtbhack.back.jooq.tables.DatasetTag;
 import ru.onemore.vtbhack.back.repository.DatasetRepository;
+import ru.onemore.vtbhack.back.repository.DatasetTagsRepository;
 
 import javax.transaction.Transactional;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 @Service
 @AllArgsConstructor
 public class DatasetCatalogService {
 
 	private final DatasetRepository datasetRepository;
+	private final DatasetTagsRepository datasetTagsRepository;
 
 	@Transactional
 	public List<DatasetEntity> getAll() {
@@ -21,7 +25,11 @@ public class DatasetCatalogService {
 	}
 
 	public FilteredRequestDatasetDTO getDefaultFilterData() {
-		return datasetRepository.getDefaultFilterData();
+		CompletableFuture<List<String>> tags = CompletableFuture.supplyAsync(() ->
+				datasetTagsRepository.getDistinctName());
+		FilteredRequestDatasetDTO filteredRequestDatasetDTO = datasetRepository.getDefaultFilterData();
+		filteredRequestDatasetDTO.setTags(tags.join());
+		return filteredRequestDatasetDTO;
 	}
 
 	@Transactional
